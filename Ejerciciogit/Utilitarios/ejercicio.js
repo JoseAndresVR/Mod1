590633ca09262f01f98f99872a0c4a1fc3af4107
Jose <jv295222@gmail.com>
2024-01-26 13:57:17 -0500

    Se agregaron las funciones Mostrar imagen y mostrar texto en caja

--- Ejerciciogit/Utilitarios/ejercicio.js
+++ Ejerciciogit/Utilitarios/ejercicio.js
@@ -17,9 +17,28 @@ recuperaciónfloat=function(ids){
     return num;
 }
 
+mostrarT = function(ids,msj){
+    let com = document.getElementById(ids);
+    com.innerText = msj;
+}
+
+mostrarI = function(ids,ruta){
+    let cmp = document.getElementById(ids);
+    cmp.src = ruta;
+}
+
+mostrarTC = function(ids,msj){
+    let nam = document.getElementById(ids);
+    nam.value = msj;
+}
+
 saludar = function(){
     let nombre=recuperación("ncm");
     let apellido = recuperación("acm");
     let edad = recuperaciónint("edcm");
     let estatura = recuperaciónfloat("escm");
+    let mensajeB = "Bienvenido "+nombre+" "+apellido;
+    mostrarT("tecm",mensajeB);  
+    mostrarI("iman","../imagenes/g3.gif");
+    mostrarTC("ncm","");
 }
