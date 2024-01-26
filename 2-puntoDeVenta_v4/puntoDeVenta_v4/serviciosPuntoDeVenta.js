285c2de7dec9a85f222883c59dea5d9cb004af22
Jose <jv295222@gmail.com>
2024-01-26 14:14:59 -0500

    Se realizaron las funciones requeridas en el archivo servicios punto de venta

---
+++ 2-puntoDeVenta_v4/puntoDeVenta_v4/serviciosPuntoDeVenta.js
@@ -0,0 +1,15 @@
+calcularValorDescuento = function(monto,porcentajeDescuento){
+    return (monto*porcentajeDescuento)/100;
+    ;
+}
+
+calcularIVA = function(montoSinIVA){
+    return (montoSinIVA*12)/100;
+}
+calcularSubTotal = function(precio,cantidad){
+    return precio*cantidad;
+}
+
+calcularTotal = function(subtotal,descuento,iva){
+    return (subtotal-descuento)+iva;
+}
