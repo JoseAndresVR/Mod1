6b6de04e5193818993a59aae95e03c7b3ab96ef5
Jose <jv295222@gmail.com>
2024-01-26 12:59:01 -0500

    Se agregaron las funciones recuperar entero y flotante para obtener la edad y estatura

--- Ejerciciogit/Utilitarios/ejercicio.js
+++ Ejerciciogit/Utilitarios/ejercicio.js
@@ -4,8 +4,22 @@ recuperación=function(ids){
     let vl = nam.value;
     return vl
 }
+recuperaciónint=function(ids){
+    console.log("Hola");
+    let nmr = document.getElementById(ids);
+    let num = parseInt(nmr.value);
+    return num;
+}
+recuperaciónfloat=function(ids){
+    console.log("Hola");
+    let nmr = document.getElementById(ids);
+    let num = parseFloat(nmr.value);
+    return num;
+}
 
 saludar = function(){
     let nombre=recuperación("ncm");
-    let apellido = recuperación("acm")
+    let apellido = recuperación("acm");
+    let edad = recuperaciónint("edcm");
+    let estatura = recuperaciónfloat("escm");
 }
