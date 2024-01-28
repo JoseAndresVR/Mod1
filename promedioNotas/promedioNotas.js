eac653fd3d639c6ec839ca03eff2d813cba42def
Jose <jv295222@gmail.com>
2024-01-27 21:34:59 -0500

    Ejercicio promedio de notas realizado con exito hasta el punto 5

---
+++ promedioNotas/promedioNotas.js
@@ -0,0 +1,11 @@
+calcularPromedioNotas = function(){
+    let nt1 = recuperarFlotante("nta1");
+    let nt2 = recuperarFlotante("nta2");
+    let nt3 = recuperarFlotante("nta3");
+    let promedio = calcularPromedio(nt1,nt2,nt3);
+    let prf = promedio.toFixed(2);
+    let mensaje = "Calificación "+prf;
+    cambiarTexto("califi",mensaje);
+
+
+}
---
+++ promedioNotas/serviciosPromedioNotas.js
@@ -0,0 +1,6 @@
+calcularPromedio = function(n1,n2,n3){
+    let promedio = (n1+n2+n3);
+    return promedio/3;
+}
+
+
