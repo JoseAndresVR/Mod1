5438832b7ebaf352303f8a8334c20b9b82fd420a
Jose <jv295222@gmail.com>
2024-02-07 16:04:25 -0500

    Paso 1 Completado

--- JuegoAhorcado/ahorcado.js
+++ JuegoAhorcado/ahorcado.js
@@ -1,4 +1,5 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+let x=0,palabraSecreta;
 esMayuscula = function(caracter){
     let x = caracter.charCodeAt();
     if(x >= 65 && x<=90){
@@ -6,4 +7,22 @@ esMayuscula = function(caracter){
     } else{
         return false;
     }
+}
+guardarPalabra = function(){
+    let pal = recuperarTexto("txtSecreta"),letra;
+    for (let i = 0; i < pal.length; i++) {
+        letra = pal.charAt(i);
+        if (esMayuscula(letra)) {
+            x++;
+        }
+        
+    }
+    if (pal.length==5 && x==5){
+        palabraSecreta = pal;
+        console.log(palabraSecreta);
+    }else{
+        alert ("Debes ingresar 5 letras mayusculas");
+    }
+    
+        
 }
