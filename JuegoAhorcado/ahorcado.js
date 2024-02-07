409acb32a044fa85b723e59eded0f8f7e6990a27
Jose <jv295222@gmail.com>
2024-02-07 16:22:31 -0500

    Paso 2 Completado

--- JuegoAhorcado/ahorcado.js
+++ JuegoAhorcado/ahorcado.js
@@ -19,10 +19,26 @@ guardarPalabra = function(){
     }
     if (pal.length==5 && x==5){
         palabraSecreta = pal;
-        console.log(palabraSecreta);
     }else{
         alert ("Debes ingresar 5 letras mayusculas");
     }
-    
-        
+}
+
+mostrarLetra=function(letra,posicion){
+    letra = recuperarTexto("txtLetra")
+    let pal2,p1=0,p2=0,p3=0,p4=0,p5=0,pa;
+    for (let i = 0; i < palabraSecreta.length; i++) {
+        pal2 = palabraSecreta.charAt(i);
+        if (letra==pal2 && i==0) {
+            mostrarTexto("div0",letra);
+        }else if (pal2==letra && i==1) {
+            mostrarTexto("div1",letra)
+        }else if (pal2==letra && i==2) {
+            mostrarTexto("div2",letra)
+        }else if (pal2==letra && i==3) {
+            mostrarTexto("div3",letra)
+        }else if (pal2==letra && i==4) {
+            mostrarTexto("div4",letra)
+        }
+    }
 }
