34a0901a1672d8104405afa468390a04d7fae29f
Jose <jv295222@gmail.com>
2024-02-07 11:50:30 -0500

    Reto 37 Completado

---
+++ Bucles/ejemplosfor.js
@@ -0,0 +1,56 @@
+mostrarNumeros = function(){
+    console.log("Antes de:");
+    for (let i = 0; i < 4; i++) {
+        console.log(i)        
+    }
+    console.log("Despues de:");
+
+}
+
+mostrarNumeros2 = function(){
+    console.log("Antes de:");
+    for (let i = 1; i <= 5; i++) {
+        console.log(i)        
+    }
+    console.log("Despues de:");
+
+}
+
+mostrarPares = function(){
+    console.log("Antes de:");
+    for (let i = 2; i <=10; i+=2) {
+        console.log(i)        
+    }
+    console.log("Despues de:");
+
+}
+
+mostrarInverso = function(){
+    console.log("Antes de:");
+    for (let i = 10; i >=0; i--) {
+        console.log(i)        
+    }
+    console.log("Despues de:");
+
+}
+
+hackearNASA = function(){
+    console.log("Antes de:");
+    for (let pr = 0; pr <=100; pr+=10) {
+        console.log("Hackeando NASA: "+pr+"%");      
+    }
+    console.log("LA NASA HA SIDO HACKEADA");
+    
+    console.log("Despues de:");
+
+}
+
+mostrarImpares = function(){
+    console.log("Antes de:");
+    for (let i = 1; i <=21; i+=2) {
+        console.log(i);   
+    }
+    console.log("Despues de:");
+
+}
+
