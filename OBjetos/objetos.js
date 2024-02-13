4cf280513c140f57e36b1b05576ac1665c49bc97
Jose Andres <jv295222@gmail.com>
2024-02-12 21:17:03 -0500

    Reto 43 Completado

---
+++ OBjetos/objetos.js
@@ -0,0 +1,23 @@
+crearProducto = function () {
+    let producto1={
+        nombre: "Dorito",
+        precio:2.30,
+        stock: 3
+    };producto2={
+        nombre: "Chitos",
+        precio:2.50,
+        stock: 3
+    }
+
+    console.log(producto1.nombre);
+    console.log(producto2.precio);
+    if (producto1.stock>producto2.stock) {
+        console.log("Producto 1 tiene mayor stock");
+    }else if (producto1.stock<producto2.stock) {
+        console.log("Producto 2 tiene mayor stock");
+    }else{
+        console.log("Ambos productos tienen el mismo stock");
+
+    }
+
+}
