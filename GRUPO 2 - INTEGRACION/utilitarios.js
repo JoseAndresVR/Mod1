54359a6551a8106ae0b417316fc58d6c5cebe15a
euronromanov <juanpablo.gaona@yahoo.com>
2024-02-24 15:53:29 -0500

    validacion de nota

--- GRUPO 2 - INTEGRACION/utilitarios.js
+++ GRUPO 2 - INTEGRACION/utilitarios.js
@@ -1,120 +1,118 @@
 
-mostrarImagen = function (idComponente, rutaImagen) {
+mostrarImagen=function(idComponente,rutaImagen){
     let componente;
-    componente = document.getElementById(idComponente);
-    componente.src = rutaImagen;
+    componente=document.getElementById(idComponente);
+    componente.src =  rutaImagen;
 }
-mostrarTexto = function (idComponente, mensaje) {
+mostrarTexto=function(idComponente,mensaje){
     let componente;
-    componente = document.getElementById(idComponente);
+    componente=document.getElementById(idComponente);
     componente.innerText = mensaje;
 }
-mostrarTextoEnCaja = function (idComponente, mensaje) {
+mostrarTextoEnCaja = function(idComponente,mensaje){
     let componente;
-    componente = document.getElementById(idComponente);
+    componente=document.getElementById(idComponente);
     componente.value = mensaje;
 }
 
-recuperarTexto = function (idComponente) {
+recuperarTexto=function(idComponente){
     let componente;
     let valorIngresado;
-    componente = document.getElementById(idComponente);
-    valorIngresado = componente.value;
+    componente=document.getElementById(idComponente);
+    valorIngresado=componente.value;
     return valorIngresado;
 }
 
-recuperarInt = function (idComponente) {
-    let valorCaja = recuperarTexto(idComponente);
-    let valorEntero = parseInt(valorCaja);
-    return valorEntero;
+recuperarInt = function(idComponente){
+   let valorCaja= recuperarTexto(idComponente);
+   let valorEntero = parseInt(valorCaja);
+   return valorEntero;
 }
 
-recuperarFloat = function (idComponente) {
-    let valorCaja = recuperarTexto(idComponente);
+recuperarFloat = function(idComponente){
+    let valorCaja= recuperarTexto(idComponente);
     let valorFlotante = parseFloat(valorCaja);
     return valorFlotante;
-}
+ }
 
-mostrarComponente = function (idComponente) {
+mostrarComponente = function(idComponente){
     document.getElementById(idComponente).style.display = "block";
 }
 
-ocultarComponente = function (idComponente) {
+ocultarComponente = function(idComponente){
     document.getElementById(idComponente).style.display = "none";
 }
 
-deshabilitarComponente = function (idComponente) {
+deshabilitarComponente = function(idComponente){
     document.getElementById(idComponente).disabled = true;
 }
 
-habilitarComponente = function (idComponente) {
+habilitarComponente = function(idComponente){
     document.getElementById(idComponente).disabled = false;
 }
-recuperarTextDiv = (idComponente) => {
+recuperarTextDiv=(idComponente)=>{
     let componente;
     let valorIngresado;
-    componente = document.getElementById(idComponente);
-    valorIngresado = componente.textContent;
+    componente=document.getElementById(idComponente);
+    valorIngresado=componente.textContent;
     return valorIngresado;
 }
 
-recuperarFloatDiv = function (idComponente) {
-    let valorCaja = recuperarTextDiv(idComponente);
+recuperarFloatDiv = function(idComponente){
+    let valorCaja= recuperarTextDiv(idComponente);
     let valorFlotante = parseFloat(valorCaja);
     return valorFlotante;
-}
-recuperarIntDiv = function (idComponente) {
-    let valorCaja = recuperarTextDiv(idComponente);
+ }
+ recuperarIntDiv = function(idComponente){
+    let valorCaja= recuperarTextDiv(idComponente);
     let valorEntero = parseInt(valorCaja);
     return valorEntero;
-}
-cambiarTexto = function (idComponente, mensaje) {
+ }
+ cambiarTexto=function(idComponente,mensaje){
     let componente;
-    componente = document.getElementById(idComponente);
-    componente.innerText = mensaje;
+    componente=document.getElementById(idComponente);
+    componente.innerText=mensaje;
+}
+
+esNombreValido=function(nombre){
+    let nombreValido=false;
+    if(nombre.legth>=3){
+            mostrarTexto("lblNombreError","");
+        return true;
+    }else{
+    mostrarTexto("lblNombreError","Debe tener al menos 3 caracteres");
+    return nombreValido;
+    }
+}
+
+estaVacia=(idComponente, valor)=>{
+    console.log(valor);
+    if ( isNaN(valor) || valor=='') {
+        mostrarTexto(idComponente,"ESTE CAMPO ES OBLIGATORIO");
+        return true;
+    }else{
+        return false;
+    }
+}
+
+estaVacia1=(idComponente, valor)=>{
+    console.log(valor);
+    if ( !isNaN(valor) || valor=='') {
+        mostrarTexto(idComponente,"ESTE CAMPO ES OBLIGATORIO");
+        return true;
+    }else{
+        return false;
+    }
 }
 
 
-esValidadTexto = function (idComponente, valor) {
-
-    let hayError = false;
-    if (!estaVacia1(idComponente, valor)) {
-        if (valor.length < 3) {
-            hayError = true;
-            mostrarTexto(idComponente, "DEBE TENER AL MENOS 3 CARACTERES");
-        }
-
-
-        esNombreValido = function (nombre) {
-            let nombreValido = false;
-            if (nombre.legth >= 3) {
-                mostrarTexto("lblNombreError", "");
-                return true;
-
-            } else {
-                mostrarTexto("lblNombreError", "Debe tener al menos 3 caracteres");
-                return nombreValido;
-            }
-        }
-
-        estaVacia = function (idComponente, valor) {
-            console.log(valor);
-            if (isNaN(valor) || valor == '') {
-                mostrarTexto(idComponente, "ESTE CAMPO ES OBLIGATORIO");
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        estaVacia1 = function (idComponente, valor) {
-            console.log(valor);
-            if (!isNaN(valor) || valor == '') {
-                mostrarTexto(idComponente, "ESTE CAMPO ES OBLIGATORIO");
-                return true;
-            } else {
-                return false;
-            }
-        }
-    }
+validarNota=(idComponente,nota)=>{
+    let hayError=false;
+   if (nota<0 || nota>10) {
+    hayError=true;
+    mostrarTexto(idComponente,"DEBE TENRE UN VALOR ENTRE 0 Y 10");
+   }else{
+    mostrarTexto(idComponente,"");
+   }
+   return hayError;
 }
