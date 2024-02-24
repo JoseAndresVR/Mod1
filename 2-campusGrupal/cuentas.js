b45d82f52ca619337388efcc27c2f8a87a00365a
Jose <jv295222@gmail.com>
2024-02-24 00:49:47 -0500

    TERMINADO CUENTAS

--- 2-campusGrupal/cuentas.js
+++ 2-campusGrupal/cuentas.js
@@ -16,14 +16,27 @@ mostrarCuentas=function(){
         Columnas: NUMERO CUENTA, NOMBRE, SALDO
         En la columna NOMBRE concatenar el nombre y el apellido
     */
+
+    let tablaCuentas = "<table><tr><th>CUENTA</th><th>NOMBRE</th><th>SALDO</th></tr>";
+    for (let i = 0; i <cuentas.length; i++) {
+        tablaCuentas += "<tr><td>"+cuentas[i].numeroCuenta+"</td><td>"+cuentas[i].nombre+" "+cuentas[i].apellido+"</td><td>"+cuentas[i].saldo+"</td></tr>";
+    }
+    tablaCuentas += "</table>";
+    mostrarTabla("inferior",tablaCuentas);
+
 }
 
 /*
     Busca la cuenta en el arreglo en función del número de cuenta,
     si existe retorna el objeto cuenta, caso contrario retorna null. 
 */
-buscarCuenta=function(numeroCuenta){
-
+buscarCuenta=function(numCuenta){
+    for(let i =0 ; i<cuentas.length;i++){
+        if (numCuenta==cuentas[i].numeroCuenta) {
+            return cuentas[i];
+        }
+    }
+    return null;
 }
 
 /*
@@ -33,6 +46,14 @@ buscarCuenta=function(numeroCuenta){
 agregarCuenta=function(cuenta){
     //Si ya existe mostrar un alert CUENTA EXISTENTE
     //Si se agrega, mostrar un alert CUENTA AGREGADA
+    if (buscarCuenta(cuenta.numeroCuenta)==null) {
+        alert("Cuenta agregada exitosamente");
+        cuentas.push(cuenta);
+
+    }else{
+        alert("Cuenta Existente");
+
+    }
 }
 
 agregar=function(){
@@ -40,4 +61,16 @@ agregar=function(){
     //Crea un objeto cuenta y agrega los atributos con los valores de las cajas respectivas
     //Invoca a agregarCuenta
     //Invoca a mostrarCuentas
+    let cuentaN=recuperarTexto("caja1"),nombreN=recuperarTexto("caja2"),apellidoN=recuperarTexto("caja4"),cedulaN=recuperarTexto("caja3"),saldoN=0;
+    let cuentaNueva={};
+    cuentaNueva.nombre = nombreN;
+    cuentaNueva.numeroCuenta = cuentaN;
+    cuentaNueva.apellido = apellidoN;
+    cuentaNueva.cedula = cedulaN;
+    cuentaNueva.saldo=saldoN;
+    agregarCuenta(cuentaNueva);
+    mostrarCuentas();
+
+
+
 }
--- 2-campusGrupal/utilitarios.js
+++ 2-campusGrupal/utilitarios.js
@@ -9,6 +9,12 @@ mostrarTexto=function(idComponente,mensaje){
     componente=document.getElementById(idComponente);
     componente.innerText = mensaje;
 }
+
+mostrarTabla=function(idComponente,mensaje){
+    let componente;
+    componente=document.getElementById(idComponente);
+    componente.innerHTML = mensaje;
+}
 mostrarTextoEnCaja = function(idComponente,mensaje){
     let componente;
     componente=document.getElementById(idComponente);
